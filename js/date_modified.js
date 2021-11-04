d8ff07c040bedfe3d8577019fce1f4f5273a1d0f
catalina-petcu <catalina.petcu3@gmail.com>
2021-11-04 20:56:41 +0100

    design principles page added

---
+++ js/date_modified.js
@@ -0,0 +1,5 @@
+var string = document.lastModified;
+let oLastModif = new Date(document.lastModified);
+let day = (oLastModif.getMonth() + 1) + "/" + oLastModif.getDate() + "/" + oLastModif.getFullYear();
+let hour = oLastModif.getHours() + ":" + oLastModif.getMinutes() + ":" + oLastModif.getSeconds();
+document.getElementById("lastModified").textContent = day + " " + hour;
--- js/date_script.js
+++
@@ -1,9 +0,0 @@
-let date = new Date();
-document.getElementById("currentYear").textContent = date.getFullYear();
-
-
-var string = document.lastModified;
-let oLastModif = new Date(document.lastModified);
-let day = (oLastModif.getMonth()+1) + "/" + oLastModif.getDate() + "/" + oLastModif.getFullYear();
-let hour = oLastModif.getHours() + ":" + oLastModif.getMinutes() + ":" + oLastModif.getSeconds();
-document.getElementById("lastModified").textContent = day + " " + hour;
