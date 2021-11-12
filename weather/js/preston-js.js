eb2e379ddb142bbed74debee50f5b7b5389a224b
catalina-petcu <catalina.petcu3@gmail.com>
2021-11-12 18:46:01 +0100

    Preston page added

---
+++ weather/js/preston-js.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+
+var today = new Date();
+var weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+var dayOfWeek = weekday[today.getDay()];
+var dayOfMonth = today.getDate();
+var months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+var curMonth = months[today.getMonth()];
+var curYear = today.getFullYear();
+var date = dayOfWeek + ", " + dayOfMonth + " " + curMonth + ", " + curYear;
+console.log(document.querySelector("#currentDate").innerHTML = date);
