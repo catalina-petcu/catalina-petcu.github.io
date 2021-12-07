bf738eefc31d65a917a91b3158431cf14637f342
catalina-petcu <catalina.petcu3@gmail.com>
2021-12-07 16:11:46 +0100

    fixed html and js

--- js/date_modified.js
+++ js/date_modified.js
@@ -2,4 +2,4 @@ var string = document.lastModified;
 let oLastModif = new Date(document.lastModified);
 let day = (oLastModif.getMonth() + 1) + "/" + oLastModif.getDate() + "/" + oLastModif.getFullYear();
 let hour = oLastModif.getHours() + ":" + oLastModif.getMinutes() + ":" + oLastModif.getSeconds();
-document.getElementById("lastModified").textContent = day + " " + hour;
+document.getElementById("lastModified").innerHTML = day + " " + hour;
