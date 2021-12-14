c3f71dc2565cef9d785505ab51af4f9da8a1e628
catalina-petcu <catalina.petcu3@gmail.com>
2021-12-14 21:25:19 +0200

    updated json file

--- scooter-project/js/prices.js
+++ scooter-project/js/prices.js
@@ -1,16 +1,9 @@
-$.getJSON("data.json", function (json) {
-    console.log(json); // this will show the info it in firebug console
-});
-/*
-const requestURL = "data/data.json"
+
+const requestURL = "https://github.com/catalina-petcu/catalina-petcu.github.io/blob/master/scooter-project/data/data.json"
 fetch(requestURL)
     .then(function (response) {
         return response.json();
     })
     .then(function (jsonObject) {
-        console.table(jsonObject); // temporary checking for valid response and data parsing
-        const info = jsonObject;
-        for (let i = 0; i < prophets.length; i++) {
-            console.log(info["Rental Type"])
-        }
-    });*/
+        console.log(jsonObject)
+    });
